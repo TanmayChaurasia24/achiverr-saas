87c253b6ba4ee977f00cb85fdc9207789f95a2ea
tanmay <kuamrchaurasiatanmay@gmail.com>
2025-03-27 14:51:40 +0530

    written temp routes

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -1,13 +1,38 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
 export const CreateTodo = async (req: Request, res: Response): Promise<any> => {
-    try {
-        
-    } catch (error) {
-        return res.status(500).json({
-            message: "error while creating the todo",
-            error
-        })
+  try {
+    const { goalId } = req.params;
+    const { description, day, completed } = req.body;
+
+    if (!goalId || !day || !description) {
+      return res.status(400).json({ message: "Please fill in all fields." });
     }
-}
+
+    const todo = await prisma.task.create({
+      data: {
+        description,
+        goalId,
+        day,
+        completed,
+      },
+    });
+
+    if (!todo) {
+      return res.status(400).json({ message: "Failed to create task." });
+    }
+
+    return res.status(201).json({
+      message: "task created successfully",
+      todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "error while creating the todo",
+      error,
+    });
+  }
+};
